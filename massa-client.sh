d51c60be9f9f8ad7f24db2f78901db8f12f68452
dex2code <90690143+dex2code@users.noreply.github.com>
2024-12-27 18:36:02 +0300

    Update massa-client.sh

--- massa-client.sh
+++ massa-client.sh
@@ -1,1 +1,23 @@
+#!/usr/bin/env bash
+
+PASS_FILE="massa-pass.txt"
+
+echo "Starting massa-client..."
+
+if [[ -z "$MASSA_PASS" ]]; then
+      echo "MASSA_PASS not set. Using $PASS_FILE"
+
+      if [[ ! -f $PASS_FILE ]]; then
+            echo "$PASS_FILE not found. Exiting..."
+            exit 1
+      fi
+
+      MASSA_PASS=$(cat $PASS_FILE)
+fi
+
+cd ~/massa-client
+./massa-client -p $MASSA_PASS
+
+echo "massa-client exited"
+exit 0
 
